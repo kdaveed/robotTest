85754d9c162fce9639d41580342a6c2619ba94ae
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-01-09 11:46:13 +0100

    test.js renamed to main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var _ = require('underscore')
+var log = require('loglevel');
+log.setLevel("debug")
+
+const testLib = require('./testLib').testLib;
+
+if (process.argv.length <= 2) {
+    console.log("Usage: ***.js directory");
+    process.exit(-1);
+}
+
+var folder = process.argv[2]
+
+var paths = testLib.getFolderVars(folder)
+var dates = testLib.getDates(paths.dates)
+
+testLib.deleteFolderRecursive(paths.result)
+testLib.createDir(paths.result)
+
+var run  = function(){
+
+  var filePath
+  fs.readdirSync(paths.root).forEach(function(folderName) {
+
+    var dir = paths.root + "/" + folderName
+    log.debug(folderName)
+    if(fs.lstatSync(dir).isDirectory()){
+        fs.readdirSync(dir).forEach(function(item){
+
+          var dateFolder = getDateFolder(dir + "/" + item)
+          log.debug("dateFolder : " + dateFolder)
+          targetDir = paths.result + "/" + dateFolder //+ "/" + folderName
+          testLib.copy(dir, targetDir, item)
+      })
+    }
+  })
+}
+
+var getDateFolder = function(filePath){
+
+  log.debug("getDateFolder : " + filePath)
+  //Iterate on input dates
+  var folder;
+  var content = fs.readFileSync(filePath, "utf8")
+  log.debug("content : " + content)
+  var results = []
+  dates.forEach(function(date){
+    results.push(testLib.getAllIndexes(content, date).length > 0)
+  })
+
+  var resultObject = _.countBy(results)
+  switch(resultObject.true){
+    case undefined :
+      return "notFound"
+    case 1 :
+      return dates[_.indexOf(results, true)]
+    default :
+      return "multiple"
+  }
+}
+
+run()
