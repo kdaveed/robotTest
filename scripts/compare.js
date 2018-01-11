dc65cf54384c83b6e3618b7ab6141b15b02b3a83
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-01-11 20:13:29 +0100

    compare works

--- scripts/compare.js
+++ scripts/compare.js
@@ -3,8 +3,12 @@
 
 var _ = require('underscore')
 
-var log = require("loglevel")
-log.setlevel("debug")
+var log = require('loglevel');
+log.setLevel("debug")
+
+const testLib = require('../testLib').testLib;
+
+
 // if (process.argv.length <= 2) {
 //     console.log("Usage: compare.js emihJSON csJSON output");
 //     process.exit(-1);
@@ -18,29 +22,30 @@ var emih = ["b", "c"]
 
 var run = function(cs, emih){
 
-  var compare = compare(Object.keys(cs.folders), Object.keys(emih.folders))
+  var diffObject = compareLists(Object.keys(cs.folders), Object.keys(emih.folders))
   var csFolders = cs.folders
   var emihFolders = emih.folders
-  compare.cases = {}
-  log.debug("compare:both: ", compare.cases)
-  compare.both.forEach(function(key){
+  diffObject.matches = {}
+  log.debug("compare:both: ", diffObject.both)
+  diffObject.both.forEach(function(key){
     csObject =  csFolders[key].files
     emihObject = emihFolders[key].files
-    compare.cases[key] = compareListsArray(csObject, emihObject)
+    diffObject.matches[key] = compareListsArray(csObject, emihObject)
   })
-  return compare
+  return diffObject
 }
 
 var compareListsArray = function(csObject, emihObject){
   var cs = Object.keys(csObject)
   var emih = Object.keys(emihObject)
+  log.debug("csKeys :", cs, "emihKeys", emih)
   var object = {
     both : _.intersection(cs, emih),
     missingFromEMIH : _.difference(cs, emih),
     missingFromCS : _.difference(emih, cs),
     sizeNotMatches : [],
   }
-
+  log.debug("compare:both: ", object.both)
   object.both.forEach(function(size){
     if(csObject[size].length !== emihObject[size].length){
       object.sizeNotMatches.push(size)
@@ -52,12 +57,10 @@ var compareListsArray = function(csObject, emihObject){
 var compareLists = function(cs, emih){
   return {
     both : _.intersection(cs, emih),
-    missingFromEMIH : _.difference(cs, emih),
-    missingFromCS : _.difference(emih, cs),
+    missingFolderFromEMIH : _.difference(cs, emih),
+    missingFolderFromCS : _.difference(emih, cs),
   }
 }
-
-
 
 var cs = {
   1 : ["x"],
@@ -89,5 +92,6 @@ var emihTest = {
   }
 }
 
-run(csTest, emihTest)
-var diff = compareLists(cs, emih)
+var result = run(csTest, emihTest)
+console.log(result)
+testLib.writeJSON("x.json", result)
