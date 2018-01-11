47925a14558408a809cc12e463afac81d3bb110b
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-01-11 15:21:06 +0100

    flips a JSON

---
+++ scripts/flipObject.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var fs = require("fs")
+
+if (process.argv.length <= 3) {
+    console.log("Usage: fileObject.js inputJSON outputJSON");
+    process.exit(-1);
+}
+
+var inputJSON = process.argv[2]
+var outputJSON = process.argv[3]
+
+var object = JSON.parse(fs.readFileSync(inputJSON,  'utf8'))
+var flipped = {}
+
+for(key in object){
+  flipped[object[key]] = key
+}
+
+fs.writeFileSync(outputJSON, JSON.stringify(flipped, null, 3))
+console.log(flipped)
