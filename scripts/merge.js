7ad11d95d912acfe89ebf0fe845a6ec38d0df4a7
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-01-11 16:53:09 +0100

    merge works

---
+++ scripts/merge.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var fs = require("fs")
+var copyDir = require("copy-dir")
+
+if (process.argv.length <= 2) {
+    console.log("Usage: merge.js destdir sDir1 sDir2 .. sDirN");
+    process.exit(-1);
+}
+
+var destdir = process.argv[2]
+var sourceDir
+
+console.log(process.argv)
+for(var i = 2; i < process.argv.length; i++){
+
+  sourceDir = process.argv[i]
+  console.log(sourceDir, destdir)
+  copyDir.sync(sourceDir, destdir)
+
+  // fs.readdirSync.forEach(function(subFolder){
+  //   if(fs.existsSync(destdir + "/" + subFolder)){
+  //     //Copy the content
+  //
+  //   })
+  // })
+}
+
+// var copyDir
+// fs.readdirSync(dir).forEach(function(subFolder){
+//   var subPath = sdir
+// })
