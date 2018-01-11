b30db0e967d9497870a21528dfdbb79c943c706b
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-01-11 15:56:44 +0100

    renameDir rename directory recursively based on input JSON file.

---
+++ scripts/renameDir.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var fs = require("fs")
+
+if (process.argv.length <= 3) {
+    console.log("Usage: renameDir.js dir nameMap");
+    process.exit(-1);
+}
+
+var dir = process.argv[2]
+var nameMap = process.argv[3]
+
+//TODO ERROR MSG for unknown input JSON file
+var map = JSON.parse(fs.readFileSync(nameMap,  'utf8'))
+
+var renameSubFolders = function(dir){
+
+  fs.readdirSync(dir).forEach(function(subFolder){
+    var subFolderPath = dir + "/" + subFolder
+    if(fs.lstatSync(subFolderPath).isDirectory()){
+      if(map[subFolder] !== undefined){
+        console.log(dir + "/" + subFolder, dir + "/" + map[subFolder])
+        fs.renameSync(subFolderPath, dir + "/" + map[subFolder])
+        renameSubFolders(dir + "/" + map[subFolder])
+      } else {
+        renameSubFolders(subFolderPath)
+      }
+    }
+  })
+}
+
+renameSubFolders(dir)
